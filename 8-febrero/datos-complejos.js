0ee5714c384621a49d4738678080fce3b6bab2a7
hanier <hsmp16@gmail.com>
2024-02-09 09:47:48 -0600

    ejercicios clase 8 Febrero

---
+++ 8-febrero/datos-complejos.js
@@ -0,0 +1,9 @@
+//Definiendo un Arreglo o Array
+//Son colecciones de datos
+
+let frutas = ["Manzana", "Pera", "Pinia"];
+frutas[0] = "Sandia";
+
+console.log(frutas);
+
+//Investigar los comandos basicos de la linea de comandos
---
+++ 8-febrero/datos-primitivos.js
@@ -0,0 +1,45 @@
+// Un comentario de una sola linea
+
+/* Llave de apertura de mi comentario 
+Hanier
+Steve
+Morales
+Perez
+*/
+
+//Tipos de Datos
+
+//Primitivos
+/*
+1. Numbers -> Para numeros
+2. Strings -> Para cadenas de texto
+3. Boolean -> Verdadero o Falso, Cero y Uno
+4. Symbol -> Para definir una llave o un valor unico
+5. Bigint -> Para cantidades sumamente grandes
+6. Null -> Para cuando no hay un valor estipulado
+7. Undefined -> Cuando algo no está definido
+*/
+
+//Complejos
+
+/*
+1. Objetos -> Coleccion de datos que poseen propiedades
+2. Arreglos -> Coleccion de datos, pueden ser Arrays de numeros o strings
+3. Funciones -> Conocidas tambien como metodos, definen un comportamiento
+*/
+
+let name = "Hanier"; //Declarando una variable
+// console.log(name);
+
+let age = 27; //Numbers
+
+let newName = "Hola soy Hanier y estudio Programacion 2do Año";
+//console.log(newName);
+
+let direccion = true; //Definiendo un booleano
+direccion = false;
+//console.log(direccion);
+
+let telefono = 85111445; //Definiendo una constante
+telefono = 85868788;
+console.log(telefono);
